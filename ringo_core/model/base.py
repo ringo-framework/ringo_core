2447d345ba8209c9b3d87808b55af7b82122a605
Torsten Irländer <torsten.irlaender@googlemail.com>
2017-09-22 17:35:21 +0200

    Added sqlalchemy columns to the base class.

--- ringo_core/model/base.py
+++ ringo_core/model/base.py
@@ -2,18 +2,29 @@
 # -*- coding: utf-8 -*-
 import uuid
 from datetime import datetime
+import sqlalchemy as sa
+
+from ringo_core.model.datatypes import UUID
 
 
 class Base(object):
     """Base for all models in Ringo"""
 
+    id = sa.Column("id", sa.Integer, primary_key=True)
+    """Unique identifier within the database. Used to load datasets
+    from the database."""
+
+    created = sa.Column("uuid", UUID)
+    """Globally unique indentifier."""
+
+    created = sa.Column("created", sa.DateTime)
+    """Datetime when the dataset was created."""
+
+    updated = sa.Column("updated", sa.DateTime)
+    """Datetime when the dataset was last modfied."""
+
     def __init__(self):
         self.id = None
-        """Unique identifier within the database. Used to load datasets
-        from the database."""
         self.uuid = uuid.uuid4()
-        """Globally unique indentifier."""
         self.created = datetime.utcnow()
-        """Datetime when the dataset was created."""
         self.updated = datetime.utcnow()
-        """Datetime when the dataset was last modfied."""
