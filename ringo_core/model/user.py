6948eb52cb4140ad97d173d77874fa083ca165a8
Torsten Irländer <torsten.irlaender@googlemail.com>
2017-09-25 15:39:10 +0200

    Renamed attribute in user model to create a user
    Replaced email with name.

--- ringo_core/model/user.py
+++ ringo_core/model/user.py
@@ -9,36 +9,34 @@ class UserFactory(object):
 
     """Factory for user objects"""
 
-    def __init__(self):
-        """TODO: to be defined1. """
-
-    def create(self, email, password):
+    def create(self, name, password):
         """Will create a new :class:`User` object. The password will be
         encrypted.
 
-        :email: E-Mail of the new user. Used as for the username.
+        :name: Name of the new user. Used as for the username on
+        authentification.
         :password: Unencrypted password of the new user.
         :returns: :class:`User` object.
 
         """
         encrypted_password = encrypt_password(password)
-        return User(email, encrypted_password)
+        return User(name, encrypted_password)
 
 
 class User(Base):
 
     """User class"""
 
-    def __init__(self, email, password):
+    def __init__(self, name, password):
         """TODO: to be defined1.
 
-        :email: E-Mail of the user used as username
+        :name: Name of the user used as username
         :password: Encrypted password of the user
 
         """
         super(User, self).__init__()
 
-        self.name = email
+        self.name = name
         """Username of the user."""
         self.password = password
         """Encrypted password of the user."""
