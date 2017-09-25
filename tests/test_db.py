54f9bc5af87a95a19e3155e3590b6c1b6fe27d4a
Torsten Irländer <torsten.irlaender@googlemail.com>
2017-09-25 15:55:08 +0200

    Added tests for the scoped session.

--- tests/test_db.py
+++ tests/test_db.py
@@ -7,6 +7,7 @@ test_db
 
 Tests for `ringo_core.lib.db` module.
 """
+import pytest
 
 
 def test_get_engine():
@@ -19,3 +20,38 @@ def test_get_dbsession():
     from ringo_core.lib.db import get_db_session
     session = get_db_session()
     assert session is not None
+
+
+def test_get_scopedsession():
+    from ringo_core.lib.db import get_db_session
+    from ringo_core.lib.db import session_scope
+    session = get_db_session()
+    with session_scope(session) as session:
+        assert session is not None
+
+
+def test_get_scopedsession_rollback():
+    from ringo_core.lib.db import get_db_session
+    from ringo_core.lib.db import session_scope
+    session = get_db_session()
+    with pytest.raises(Exception):
+        with session_scope(session) as session:
+            session.execute("Foo")
+    assert session is not None
+
+
+def test_get_scopedsession_detach_exception(db):
+    from sqlalchemy.orm.exc import DetachedInstanceError
+    from ringo_core.lib.db import session_scope
+    from ringo_core.model.base import Base
+    with pytest.raises(DetachedInstanceError):
+        with session_scope(db) as session:
+            factory = Base.get_factory()
+            new = factory.create()
+            session.add(new)
+            session.flush()
+
+        # No outside the scope. Session has been already closed and the
+        # instance is detached from the session. I expect
+        # a exection when accessing any attribute of the instance.
+        assert new.id is not None
