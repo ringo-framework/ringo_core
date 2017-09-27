d79af7944f3433598c79372451b9363a8b0c488c
Torsten Irländer <torsten.irlaender@googlemail.com>
2017-09-27 20:02:49 +0200

    Added API for users.

---
+++ ringo_core/api/user.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Public API of the user model"""
+from ringo_core.lib.db import get_db_session, session_scope
+from ringo_core.model.user import User
+from ringo_core.api.crud import (
+    _create,
+    _read,
+    _update,
+    _delete
+)
+
+
+def create(name, password):
+    """Creates a new user with the given `name` and `password`.
+
+    .. seealso:: Methods :func:`ringo_core.api.crud._create`
+
+    :name: Name of the new user
+    :password: Password (unencrypted) of the new user
+    :returns: :class:`User` instance
+
+    >>> import ringo_core.api.user
+    >>> user = ringo_core.api.user.create(name="foo1", password="bar")
+    >>> user.name
+    'foo1'
+    """
+    with session_scope(get_db_session(), close=False) as db:
+        user = _create(db, User, dict(name=name, password=password))
+    return user
+
+
+def read(item_id):
+    """Read (load) a existing user from the database.
+
+    .. seealso:: Methods :func:`ringo_core.api.crud._read`
+
+    :item_id: ID of the user to load.
+    :returns: :class:`User` instance
+
+
+    >>> import ringo_core.api.user
+    >>> # First create a new user.
+    >>> newuser = ringo_core.api.user.create(name="foo2", password="bar")
+    >>> # Now load the user
+    >>> loaduser = ringo_core.api.user.read(item_id = newuser.id)
+    >>> loaduser.name
+    'foo2'
+    """
+    with session_scope(get_db_session(), close=False) as db:
+        return _read(db, User, item_id)
+
+
+def update(item_id, values):
+    """Update a user with the given values in the database.
+
+    .. seealso:: Methods :func:`ringo_core.api.crud._update`
+
+    :item_id: ID of the user to update
+    :values: Dictionary of values
+    :returns: :class:`User` instance
+
+    >>> import ringo_core.api.user
+    >>> # First create a new user.
+    >>> newuser = ringo_core.api.user.create(name="foo3", password="bar")
+    >>> # Now load the user
+    >>> values = {"name": "baz"}
+    >>> updateduser = ringo_core.api.user.update(item_id = newuser.id, values=values)
+    >>> updateduser.name
+    'baz'
+    """
+    with session_scope(get_db_session(), close=False) as db:
+        return _update(db, User, item_id, values)
+
+
+def delete(item_id):
+    """Deletes a user from the database.
+
+    .. seealso:: Methods :func:`ringo_core.api.crud._delete`
+
+    :item_id: ID of the user to update
+
+    >>> import ringo_core.api.user
+    >>> # First create a new user.
+    >>> newuser = ringo_core.api.user.create(name="foo4", password="bar")
+    >>> # Now delete the user
+    >>> ringo_core.api.user.delete(item_id = newuser.id)
+    >>> # Check that the user was actually deleted.
+    >>> loaduser = ringo_core.api.user.read(item_id = newuser.id)
+    Traceback (most recent call last):
+        ...
+    sqlalchemy.orm.exc.NoResultFound: No row was found for one()
+    """
+    with session_scope(get_db_session()) as db:
+        return _delete(db, User, item_id)
--- tests/test_user.py
+++ tests/test_user.py
@@ -11,23 +11,53 @@ Tests for `ringo_core.model.user` module.
 import pytest
 
 
-@pytest.fixture
-def user_factory(db):
-    from ringo_core.model.user import User
-    return User.get_factory(db)
-
-
-def test_create(user_factory):
-    user = user_factory.create("foo@example.com", "mysecurepassword")
-    assert user.name == "foo@example.com"
-    assert user.password != "mysecurepassword"
-
-
-def test_create_unique_name(db, user_factory):
+def test_not_found():
     import sqlalchemy as sa
-    user1 = user_factory.create("foo@example.com", "mysecurepassword")
-    db.add(user1)
-    user2 = user_factory.create("foo@example.com", "mysecurepassword")
-    db.add(user2)
+    import ringo_core.api.user
+    with pytest.raises(sa.orm.exc.NoResultFound):
+        ringo_core.api.user.read(9999)
+
+
+def test_create(randomstring):
+    import ringo_core.api.user
+    name = randomstring(8)
+    user = ringo_core.api.user.create(name=name, password="password")
+    assert user.name == name
+
+
+def test_create_unique_name(randomstring):
+    import sqlalchemy as sa
+    import ringo_core.api.user
+    name = randomstring(8)
+    user = ringo_core.api.user.create(name=name, password="password")
+    assert user.name == name
     with pytest.raises(sa.exc.IntegrityError):
-        db.commit()
+        user = ringo_core.api.user.create(name=name, password="password")
+
+
+def test_read(randomstring):
+    import ringo_core.api.user
+    name = randomstring(8)
+    user = ringo_core.api.user.create(name=name, password="password")
+    loaded = ringo_core.api.user.read(user.id)
+    assert loaded.name == name
+
+
+def test_update(randomstring):
+    import ringo_core.api.user
+    name = randomstring(8)
+    user = ringo_core.api.user.create(name=name, password="password")
+    values = {"name": "updated"}
+    updated = ringo_core.api.user.update(user.id, values)
+    assert updated.name == "updated"
+    assert updated.updated != user.updated
+
+
+def test_delete(randomstring):
+    import sqlalchemy as sa
+    import ringo_core.api.user
+    name = randomstring(8)
+    user = ringo_core.api.user.create(name=name, password="password")
+    ringo_core.api.user.delete(user.id)
+    with pytest.raises(sa.orm.exc.NoResultFound):
+        user = ringo_core.api.user.read(9999)
