921402cb5bc18c5b02b4a343f6983c9356681077
Torsten Irländer <torsten.irlaender@googlemail.com>
2017-11-10 16:10:03 +0100

    Added JWT as requirement

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ with open('HISTORY.rst') as history_file:
 requirements = [
     "ringo_service",
     "ringo_storage",
-    "passlib"
+    "passlib",
+    "pyjwt"
     # TODO: put package requirements here
 ]
 
